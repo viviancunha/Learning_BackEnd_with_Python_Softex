932e2b69b3973cc2b8bff2eb0b0a6aeb804988bd
Vívian Cunha <vivianfcsilva@gmail.com>
2025-08-09 19:14:26 +0000

    Atualização dos exercícios 02 ao 05

--- Exercícios/02_ao_05.py
+++ Exercícios/02_ao_05.py
@@ -1,1 +1,23 @@
-# 
+# 02. Mensagens múltiplas
+#Mostre três frases diferentes usando três comandos print() separados. 
+
+print('O céu é azul.' "\n"'A grama é verde.' "\n"'A água é transparente.')
+
+# 03. Texto com quebra de linha e tabulação
+# Mostre o seguinte texto formatado: 'Nome: João'; 'Idade: 20 anos'; 'Curso: Python Básico'.
+
+print('Nome: João', "\n"'Idade: 20 anos', "\n"'Curso: Python Básico')
+
+# 04. Combinar textos e números
+# Crie variáveis para nome, idade e curso. Use print() para mostrar uma frase completa com esses dados.
+
+Nome = 'Vívian'
+Idade = 26
+Curso = 'Desenvolvimento Web com Python'
+
+print('Meu nome é', Nome +',','tenho', Idade,'anos', 'e nunca pensei que um dia poderia me interessar em aprender', Curso+'.', 'Mas estou empolgada com a possibilidade de experimentar novas tecnologias e aprender mais sobre o mundo da programação.')
+
+# 05. Desenho com caracteres
+# Use print() para desenhar um quadrado com #
+
+print('########', "\n"'#      #', "\n"'#      #"', "\n"'########')
